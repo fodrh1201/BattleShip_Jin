11602c481f80f771aee9e4f5c569ce4cc0311baf
Hyungjin Ko <fodrh1201@gmail.com>
2014-07-09 18:43:30 +0900

    input

--- BattleShip_고형진/BattleShip_PP/BattleShip_PP.cpp
+++ BattleShip_고형진/BattleShip_PP/BattleShip_PP.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
-
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+	srand((unsigned int)time(NULL));
+	char* result_arr[3] = { "hit", "miss", "distroy" };
+	
+	int input_x, input_y;
+	std::cin >> input_x;
+	std::cin >> input_y;
+	fflush(stdin);
+
+
+	int result = rand() % 3;
+	printf_s("%s", result_arr[result]);
+
+	getchar();
+
 }
 
